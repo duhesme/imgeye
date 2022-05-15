cded73b0c6ea32f56d78205b96aab53df32351bd
duhesme <hitechwatchout@gmail.com>
2022-05-15 21:02:05 +0300

    FavoriteViewController: set up favoritesTableView.

--- imgeye/Controller/FavoriteViewController.swift
+++ imgeye/Controller/FavoriteViewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SkeletonView
 
 class FavoriteViewController: UIViewController {
     
@@ -14,12 +15,43 @@ class FavoriteViewController: UIViewController {
     
     var userManager = UserManager()
     
+    var favoritesArray = [PhotoModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         userManager.downloadUser(byUsername: "guillaumevdn")
         
         favoritesTableView.register(FavoritesTableViewCell.nib, forCellReuseIdentifier: FavoritesTableViewCell.identifier)
+        favoritesTableView.dataSource = self
+    }
+    
+}
+
+extension FavoriteViewController: SkeletonTableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoritesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoritesTableViewCell.identifier, for: indexPath) as! FavoritesTableViewCell
+        
+        return cell
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoritesArray.count
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UITableView, skeletonCellForRowAt indexPath: IndexPath) -> UITableViewCell? {
+        let cell = skeletonView.dequeueReusableCell(withIdentifier: FavoritesTableViewCell.identifier, for: indexPath) as! FavoritesTableViewCell
+        
+        return cell
+    }
+    
+    func collectionSkeletonView(_ skeletonView: UITableView, cellIdentifierForRowAt indexPath: IndexPath) -> ReusableCellIdentifier {
+        return FavoritesTableViewCell.identifier
     }
     
 }
