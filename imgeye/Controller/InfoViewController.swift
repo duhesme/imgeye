66c8cc7d05ffa9924e5b4e836a89ea19273107ae
duhesme <hitechwatchout@gmail.com>
2022-05-17 14:05:25 +0300

    Add UI for 'likes' and 'downloads' in InfoView.

--- imgeye/Controller/InfoViewController.swift
+++ imgeye/Controller/InfoViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
+    @IBOutlet weak var imageContainerView: UIView!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     var photo: PhotoModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let photo = photo else {
+            return
+        }
+
+        photoImageView.kf.setImage(with: photo.urls.regular)
+        
+        imageContainerView.layer.masksToBounds = false
+        imageContainerView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        imageContainerView.layer.shadowColor = UIColor.black.cgColor
+        imageContainerView.layer.shadowOpacity = 0.43
+        imageContainerView.layer.shadowRadius = 16
+        imageContainerView.layer.cornerRadius = 0
     }
     
 }
---
+++ imgeye/Extensions/UIView+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIView+Extensions.swift
+//  imgeye
+//
+//  Created by Никита Владимирович on 17.05.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+}
